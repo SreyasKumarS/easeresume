167cddb5d66330c3c283de5bd67cc4bafadb7528
SreyasKumarS <sreyassree3333@gmail.com>
2025-04-06 16:35:35 +0530

    vercel

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,10 @@ const app = express();
 
 // CORS setup
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true,
+  origin: [process.env.FRONTEND_URL],  
+  credentials: true,              
 };
+
 app.use(cors(corsOptions));
 
 // Middleware
